93db87ff5bab04865e870d79d24f56df18882fce
Александр <alex.tolstuchin@gmail.com>
2020-05-03 20:32:41 +0500

    Change type="password" in LoginView on LoginViewModel DataType.Password

--- SimpleBlog/ViewModels/LoginViewModel.cs
+++ SimpleBlog/ViewModels/LoginViewModel.cs
@@ -1,9 +1,12 @@
-﻿namespace SimpleBlog.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleBlog.ViewModels
 {
     public class LoginViewModel
     {
         public string UserName { get; set; }
 
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         public string ReturnUrl { get; set; }
